5e6a019c503a8002bbd431eeb63b4bc107e88434
sevastyanpanov <sevastyanpanov@gmail.com>
2026-08-23 00:53:19 +0300

    hooks/branch-name-for-commits: add githook for everyone whoever uses project

--- build-logic/convention/src/main/kotlin/com/panov/sevastyan/ndpa/buildlogic/ProjectExtensions.kt
+++ build-logic/convention/src/main/kotlin/com/panov/sevastyan/ndpa/buildlogic/ProjectExtensions.kt
@@ -5,6 +5,9 @@ import org.gradle.api.artifacts.MinimalExternalModuleDependency
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
+
+private const val INSTALL_GIT_HOOKS_PATH = ":installGitHooks"
 
 internal val Project.libs: VersionCatalog
     get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
@@ -14,3 +17,9 @@ internal fun VersionCatalog.versionInt(alias: String): Int =
 
 internal fun VersionCatalog.library(alias: String): MinimalExternalModuleDependency =
     findLibrary(alias).get().get()
+
+internal fun Project.installGitHooksBeforeKotlinCompilation() {
+    tasks.withType(KotlinCompilationTask::class.java).configureEach {
+        dependsOn(INSTALL_GIT_HOOKS_PATH)
+    }
+}
